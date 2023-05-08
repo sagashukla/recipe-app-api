b8e51bbec7f3c8dbeabe55a6e8d49294089a2e36
Sagar Shukla <sagars-v@herbalife.com>
2023-05-08 11:18:38 +0530

    updating

--- app/core/models.py
+++ app/core/models.py
@@ -11,8 +11,18 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_field):
-        user = self.model(email = email, **extra_field)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email = self.normalize_email(email), **extra_field)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
